013947881cd121d03376beed9879950784fa5299
+ <+>
2023-08-16 17:37:05 +0300

    completed assgnment

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+console.log(user)
 
 function App() {
   const user = {
-    name: "Martin Abwaka",
-    city: "LA",
-    bio: "I am A Software Developer",
-    github: "https://github.com/Martoh003",
-    linkedin:"https://www.linkedin.com/in/martin-abwaka-b98a12229/"
-  };
-
+      name: 'Liza',
+      city: 'New York',
+      bio: 'I made this!',
+      color: 'firebrick',
+      links: {
+        github: 'https://github.com/liza',
+        linkedin: 'https://www.linkedin.com/in/liza/'
+      }
+    };
   return (
     <div>
       <NavBar />
